287a2b9b972057744ab866ec80d365e259f53a1f
Devifish <devifish@outlook.com>
2020-10-28 17:07:41 +0800

    update: 状态栏透明及统一使用IOS主题

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:readme/common/constant.dart';
 import 'package:readme/view/home.dart';
 
@@ -9,13 +12,26 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var systemUiStyle = SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    );
+
+    // 设置SystemUi样式
+    SystemChrome.setSystemUIOverlayStyle(systemUiStyle);
     return MaterialApp(
       title: ApplicationConstant.title,
       theme: ThemeData(
         primarySwatch: Colors.cyan,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        platform: TargetPlatform.iOS
       ),
       home: Home(title: ApplicationConstant.title),
+      debugShowCheckedModeBanner: false
     );
   }
 }
