9ed86b71db0ea890f049b2b891b3d4ee12addf2a
Devifish <devifish@outlook.com>
2020-10-29 17:37:17 +0800

    add: 添加书架列表

--- lib/view/book/bookshelf.dart
+++ lib/view/book/bookshelf.dart
@@ -21,14 +21,6 @@ class _BookShelfState extends State<BookShelf> {
     super.initState();
   }
 
-  /// 书籍列表Item
-  /// 用于批量构建书籍列表 Item 组件
-  Widget bookListItem(int index) {
-    return ListTile(
-      title: Text("书籍$index"),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,7 +38,7 @@ class _BookShelfState extends State<BookShelf> {
             itemBuilder: (context, index) {
               return GestureDetector(
                 behavior: HitTestBehavior.opaque,
-                child: bookListItem(index),
+                child: _BookListItem(key: Key(index.toString())),
                 onTap: () {
                   log("点击了 $index");
                 },
@@ -56,6 +48,66 @@ class _BookShelfState extends State<BookShelf> {
         controller: _refreshController,
         enablePullDown: true,
         onRefresh: () => {},
+      ),
+    );
+  }
+}
+
+/// 书籍列表Item
+/// 用于批量构建书籍列表 Item 组件
+class _BookListItem extends StatelessWidget {
+  final Key key;
+
+  _BookListItem({@required this.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: key,
+      margin: const EdgeInsets.only(top: 8, left: 8, right: 8),
+      child: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 128,
+                width: 96,
+                padding: const EdgeInsets.only(left: 8, top: 8),
+                color: Colors.grey,
+                child: Stack(
+                  children: [],
+                ),
+              )
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 8, top: 8.0),
+                child: Text(
+                  "书名",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 8, top: 12.0),
+                child: Text(
+                  "章节数据",
+                  style: TextStyle(fontSize: 12),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 8, top: 22.0),
+                child: Text("阅读数据",
+                    style: TextStyle(color: Colors.grey, fontSize: 11)),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
