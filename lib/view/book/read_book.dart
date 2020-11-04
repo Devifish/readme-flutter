2ab23c56fb4095690bacfe0e7c1c8ab26ba290c0
Devifish <devifish@outlook.com>
2020-11-04 18:38:18 +0800

    update: 一些调整

--- lib/view/book/read_book.dart
+++ lib/view/book/read_book.dart
@@ -9,8 +9,9 @@ class ReadBook extends StatefulWidget {
 }
 
 class _ReadBookState extends State<ReadBook> {
-  bool _showTopWidget = false;
+  bool _showReadTopMenu = false;
   List<BottomNavigationBarItem> _bottomItems;
+  String _content;
 
   @override
   void initState() {
@@ -26,11 +27,13 @@ class _ReadBookState extends State<ReadBook> {
         label: "设置",
       )
     ];
+    _content =
+        "书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书书籍内容书籍内容书籍内容";
   }
 
   void _changeShowTopWidget() {
     setState(() {
-      _showTopWidget = !_showTopWidget;
+      _showReadTopMenu = !_showReadTopMenu;
     });
   }
 
@@ -45,30 +48,47 @@ class _ReadBookState extends State<ReadBook> {
           child: GestureDetector(
             behavior: HitTestBehavior.opaque,
             onTap: _changeShowTopWidget,
-            child: Text(
-              "书籍内容书\r\n籍内容书籍内容书籍内容书籍内\r\n\r\n容书籍内容书籍内容书籍内容书籍\r\n容书籍内容书籍内容书籍内容书\r\n籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍容书籍内容书籍内容书籍内容书籍内容书籍内容书籍内容书籍内容",
-              style: TextStyle(fontSize: 15),
-            ),
+            child: _flipReadWidget(),
           ),
         ),
-        Offstage(
-          offstage: !_showTopWidget,
-          child: Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(title: Text("阅读页面"), actions: [
-                PopupMenuButton(
-                  itemBuilder: (context) => [],
-                )
-              ]),
-              body: GestureDetector(onTap: _changeShowTopWidget),
-              bottomNavigationBar: BottomNavigationBar(
-                unselectedItemColor: Colors.black,
-                selectedItemColor: Colors.black,
-                items: _bottomItems,
-                type: BottomNavigationBarType.fixed,
-              )),
-        )
+        Offstage(offstage: !_showReadTopMenu, child: _readTopMenu())
       ],
     );
   }
+
+  /// 阅读页面顶层菜单
+  ///
+  Widget _readTopMenu() {
+    return Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(title: Text("阅读页面"), actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [],
+          )
+        ]),
+        body: GestureDetector(onTap: _changeShowTopWidget),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.black,
+          items: _bottomItems,
+          type: BottomNavigationBarType.fixed,
+        ));
+  }
+
+  Widget _flipReadWidget() {
+    int fontSize = 16;
+    double screenHeight = ScreenUtil.getScreenH(context);
+
+    return PageView.builder(
+        physics: AlwaysScrollableScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) {
+          return Text(
+            _content,
+            style: TextStyle(fontSize: fontSize.toDouble()),
+          );
+        },
+        itemCount: 5);
+  }
+
+  Widget _scrollReadWidget() {}
 }
